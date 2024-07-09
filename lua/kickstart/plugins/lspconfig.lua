5df6612bb4758e8e5b674df4e2fa038d67e0b49f
alekgr <alek.grigorian@gmail.com>
2024-07-08 21:10:40 -0500

    added the other formatters..

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -201,6 +201,10 @@ return { -- LSP Configuration & Plugins
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
+      'isort',
+      'black',
+      'prettier',
+      'prettierd',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
