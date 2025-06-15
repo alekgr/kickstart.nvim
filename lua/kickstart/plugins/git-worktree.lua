db8a55f5aac3f72eea885819a767a254fe6d08cc
alekgr <alek.grigorian@gmail.com>
2025-06-15 10:39:38 -0500

    using opts now

--- lua/kickstart/plugins/git-worktree.lua
+++ lua/kickstart/plugins/git-worktree.lua
@@ -1,6 +1,6 @@
 return {
   'ThePrimeagen/git-worktree.nvim',
-  config = function()
+  opts = function()
     require('git-worktree').setup(require('telescope').load_extension 'git_worktree')
     vim.keymap.set('n', '<leader>gws', ':Telescope git_worktree git_worktrees<CR>')
     vim.keymap.set('n', '<leader>gwc', ':Telescope git_worktree create_git_worktree<CR>')
