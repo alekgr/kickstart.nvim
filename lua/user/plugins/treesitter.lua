8e269c2153f2ccb7a3cad208d92bcfb72e61b939
alekgr <alek.grigorian@gmail.com>
2026-04-07 16:27:01 -0500

    enabled treesitter indent and disabled ruby

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -7,14 +7,10 @@ return {
     auto_install = false,
     highlight = {
       enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
+                },
     indent = { enable = true, disable = { 'ruby' } },
   },
-  opts = function(_, opts)
+config = function(_, opts)
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
     -- Prefer git instead of curl in order to improve connectivity in some environments
