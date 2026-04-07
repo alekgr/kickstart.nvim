b10d739ef0c3b48428410880258eaad9b0521fde
alekgr <alek.grigorian@gmail.com>
2026-04-07 16:25:16 -0500

    changed keybindings some of telescope commands

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -80,16 +80,16 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'telescope [S]earch [H]elp' })
     vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'telescope [S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = 'telescope [S]earch [S]elect Telescope' })
+    vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'telescope [S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'telescope [S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'telescope [S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = 'telescope[S]earch [R]esume' })
+    vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = 'telescope [S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'teelescope [ ] Find existing buffers' })
     vim.keymap.set('n', '<leader>fn', function()
       builtin.find_files(require('telescope.themes').get_dropdown {
         previewer = true,
@@ -97,6 +97,18 @@ return { -- Fuzzy Finder (files, lsp, etc)
         prompt_title = 'Notes',
       })
     end, { desc = 'find notes' })
+    
+    vim.keymap.set('n', '<leader>fm', function ()
+	    require('telescope.builtin').find_files({
+		    search_dirs = {
+			    vim.fn.getcwd(),
+			    vim.fn.stdpath("config")
+		    },
+		    hidden = true,
+		    prompt_title = "Multi-directory Search",
+	    })
+    end, { desc = 'telescope find files in home direcotries and inclding configs' })
+
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
