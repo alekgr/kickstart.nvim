b54cb32995db227b6460c8e12ab44850d3893d57
alekgr <alek.grigorian@gmail.com>
2026-04-07 15:19:55 -0500

    changed clangd to use gcc and falback LLVM, use std=c89 and have more root_markers

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,12 +1,21 @@
 return {
   cmd = {
-    'clangd',
-    '--background-index',
-    '--query-driver=/usr/bin/gcc,/usr/bin/g++',
-    '--completion-style=detailed',
+    "clangd",
+    "--background-index",
+    "--fallback-style=LLVM",
+    "--query-driver=/usr/bin/gcc",
+    "--index-file=.cache/clangd/index.idx",
+    "--header-insertion=never",
+    "--clang-tidy"
+      },
+  init_options = {
+	"--fallback-flags=-std=c89"
   },
   filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
-  root_markers = { 'compile_commands.json', 'compile_flags.txt', '.git' },
+  root_markers = { 'compile_commands.json', 'compile_flags.txt', '.git', 'makefile', 'Makefile'},
+  capabilities = {
+	  offsetEncoding = { "utf-16" },
+  },
   -- This runs whenever clangd attaches to a buffer
   on_attach = function(client, bufnr)
     -- Enable native auto-completion for this buffer
@@ -15,6 +24,6 @@ return {
     end
     -- You can also put clangd-specific keymaps here
     local opts = { buffer = bufnr }
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    --vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
   end,
 }
