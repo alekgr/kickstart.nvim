28ad5f95fbf89a31093259f964a800e3dd4a05af
Alek Grigoiran(alekgr) <alek.grigorian@gmail.com>
2026-04-09 13:43:46 -0500

    added treesitter back in

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -14,3 +14,6 @@ require ("user.plugins.gitsigns")
 
 vim.pack.add({ "https://github.com/echasnovski/mini.nvim" })
 require ("user.plugins.mini")
+
+vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter"})
+require ("user.plugins.treesitter")
---
+++ lua/user/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then return end
+
+configs.setup({
+  -- A list of parser names, or "all"
+  ensure_installed = { "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  highlight = {
+    enable = true, -- This is what makes TokyoNight look amazing
+    additional_vim_regex_highlighting = false,
+  },
+  
+  indent = {
+    enable = true -- Better indentation based on code structure
+  },
+})
