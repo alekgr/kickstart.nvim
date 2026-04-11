248e30ddfb615230c7c054f9503788cc830b792f
Alek Grigoiran(alekgr) <alek.grigorian@gmail.com>
2026-04-11 17:02:26 -0500

    added a autocmd to fix the color on GutterMark

--- init.lua
+++ init.lua
@@ -21,3 +21,23 @@ require("user.plugins")
 vim.lsp.enable("pyright")
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("clangd")
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    -- Your transparency settings (keep these here too)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+
+    -- YOUR MARK COLORS (This is the important part)
+    vim.api.nvim_set_hl(0, "GutterMarksGlobal", { fg = "#ff9e64", bold = true })
+    vim.api.nvim_set_hl(0, "GutterMarksLocal", { fg = "#7aa2f7" })
+  end,
+})
+
+-- Trigger it immediately for the current session
+vim.cmd("colorscheme " .. vim.g.colors_name)
+
+-- Force backtick and single quote to be native
+vim.keymap.set('n', '`', '`', { noremap = true })
+vim.keymap.set('n', "'", "'", { noremap = true })
