2c226e22560a0b2b1fa0369fa6a759f35f7c477b
Alek Grigoiran(alekgr) <alek.grigorian@gmail.com>
2026-04-12 20:37:45 -0500

    add friendly-snippets to work with blink

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -2,6 +2,7 @@ vim.pack.add({ "https://github.com/folke/tokyonight.nvim" })
 require ("user.plugins.tokyonight")
 
 vim.pack.add({
+	{ src = "https://github.com/rafamadriz/friendly-snippets"},
 	{ src = "https://github.com/saghen/blink.cmp", version = "v1" }
 })
 require ("user.plugins.blink")
