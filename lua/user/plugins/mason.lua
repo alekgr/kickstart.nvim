c362bed9a8bde8b8145931901f175012af0582e7
Alek Grigoiran(alekgr) <alek.grigorian@gmail.com>
2026-04-17 12:10:48 -0500

    configured mason correctly

--- lua/user/plugins/mason.lua
+++ lua/user/plugins/mason.lua
@@ -1,9 +1,12 @@
+local servers = { "clangd", "lua-language-server", "pyright"}
+
+
 require ("mason").setup()
 
-opts = {
-	ensure_installed = {
-	"clangd",
-	"lua-language-server",
-	"pyright",
-	}
-}
+local mr = require("mason-registry")
+for _, tool in ipairs(servers) do
+  local p = mr.get_package(tool)
+  if not p:is_installed() then
+    p:install()
+  end
+end
